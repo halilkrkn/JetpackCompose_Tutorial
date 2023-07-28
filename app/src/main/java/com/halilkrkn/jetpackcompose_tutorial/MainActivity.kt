f89a9a4d228bc15bb4f00e4930d7e5959fb31f25
halilkrkn <Halilkrkn000@gmail.com>
2023-07-28 16:46:45 +0300

    Text Customization

--- app/src/main/java/com/halilkrkn/jetpackcompose_tutorial/MainActivity.kt
+++ app/src/main/java/com/halilkrkn/jetpackcompose_tutorial/MainActivity.kt
@@ -3,13 +3,25 @@ package com.halilkrkn.jetpackcompose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.halilkrkn.jetpackcompose_tutorial.ui.theme.JetpackCompose_TutorialTheme
 
 class MainActivity : ComponentActivity() {
